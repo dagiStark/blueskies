ee5f323a5a3f3b0f70b68db706c578caabc8f4f3
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-05 15:05:05 +0300

    create info section and add to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/navbar/Navbar'
 import Hero from './components/hero/Hero'
 import Search from './components/search/Search'
+import Info from './components/info/Info'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Navbar />
       <Hero />
       <Search />
+      <Info />
    
     </Fragment>
   )
---
+++ src/components/info/Info.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from "react"
+
+
+const Info = () => {
+  return (
+    <Fragment>
+        {/* upper section */}
+        <section>
+            <p>travel support</p>
+            <p>Plan Your Travel with confidence</p>
+            <p>Find help with booking and travel plans, see what to expect along the journey</p>
+        </section>
+      {/* info section */}
+
+      
+    </Fragment>
+  )
+}
+
+export default Info

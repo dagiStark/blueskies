6dca9ad0b62c8a435c602b509bf906e7020bdf8d
dagiStark <160214504+dagiStark@users.noreply.github.com>
2024-11-05 11:53:14 +0300

    update search

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,12 +1,40 @@
-import { Fragment } from "react"
+import { Fragment } from "react";
+import { searchItems } from "@/constants";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 const Search = () => {
   return (
     <Fragment>
-        
-      
-    </Fragment>
-  )
-}
+      {/* filter section */}
+      <section className="flex flex-col items-center justify-center gap-y-10 py-20 px-10">
+        <div className="flex items-center justify-between gap-x-10 py-4 px-24 rounded-full w-1/2 bg-black/10 text-gray-600">
+          <p>Economy</p>
+          <p>Business Class</p>
+          <p>First Class</p>
+        </div>
+      </section>
+      {/* search section */}
+      <section>
+        <div>
+          {searchItems.map((item, index) => (
+            <div
+              key={index}
+              className="flex items-center justify-between gap-x-10 py-4 px-24 rounded-full w-1/2 bg-black/10 text-gray-600"
+            >
+              <p>{item.name}</p>
+              <Input placeholder={item.placeholder} />
+              <item.icon />
+            </div>
+          ))}
+        </div>
 
-export default Search
+        <div>
+          <Button className="bg-customBg">Search Flight</Button>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+
+export default Search;
